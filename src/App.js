84b18167c3d8ebb508ac1418e7e9e54d6ea48378
Veronica Ramirez <55036002+VeroElaine@users.noreply.github.com>
2019-12-15 16:36:53 -0500

    test

--- src/App.js
+++ src/App.js
@@ -61,7 +61,6 @@ class App extends React.Component {
         <Button variant="link">Link</Button>
       </ButtonToolbar>
       <Navigation/>
-      <ParentComponent/>
 
       {this.state.habits_list.map((habits) =>(
       <Habit
@@ -76,33 +75,7 @@ class App extends React.Component {
   }
 }
 
-class ParentComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: "1990-06-05",
-      format: "YYYY-MM-DD",
-      inputFormat: "DD/MM/YYYY",
-      mode: "date"
-    };
-  }
 
-  handleChange = (newDate) => {
-    console.log("newDate", newDate);
-    return this.setState({date: newDate});
-  }
-
-  render() {
-    const {date, format, mode, inputFormat} = this.state;
-    return <DateTimeField
-      dateTime={date}
-      format={format}
-      viewMode={mode}
-      inputFormat={inputFormat}
-      onChange={this.handleChange}
-    />;
-  }
-}
 
 
 
