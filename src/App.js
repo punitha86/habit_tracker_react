04dc36245be74ba92dfffb64297d240dc81679d0
Veronica Ramirez <55036002+VeroElaine@users.noreply.github.com>
2019-12-15 01:20:17 -0500

    added buttons

--- src/App.js
+++ src/App.js
@@ -18,12 +18,26 @@ class App extends React.Component {
   render () {
     return (
       <div className="large-container">
-<h1>Hi</h1>
+        <h1>Hi</h1>
+        <ButtonToolbar>
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="success">Success</Button>
+          <Button variant="warning">Warning</Button>
+          <Button variant="danger">Danger</Button>
+          <Button variant="info">Info</Button>
+          <Button variant="light">Light</Button>
+          <Button variant="dark">Dark</Button>
+          <Button variant="link">Link</Button>
+        </ButtonToolbar>
 
       </div>
+
     )
   }
 }
+
+
 
 // =============================
 // EXPORT
